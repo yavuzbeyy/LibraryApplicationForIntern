8db62af788e228abf110cdcc8e25f6131b628c0d
Yavuz <devyavuz81@gmail.com>
2024-05-27 10:34:47 +0300

    BugFixes

--- Katmanli.API/Controllers/AuthorController.cs
+++ Katmanli.API/Controllers/AuthorController.cs
@@ -1,12 +1,14 @@
 ﻿using Katmanli.DataAccess.DTOs;
 using Katmanli.Service.Interfaces;
 using Katmanli.Service.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
 
 namespace Katmanli.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AuthorController : ControllerBase
